82185a5643a797297c2902e2527a668f2ac3f45b
Daniel Moniz <daniel.moniz@gmail.com>
2019-04-07 07:27:46 -0400

    Add API object. Call Star Wars API.

--- components.js
+++ components.js
@@ -3,9 +3,42 @@
 (function() {
   console.log('Loaded components!');
 
-  window.customElements.define('sw-search-box', class extends HTMLElement {
-    connectedCallback() {
-      this.innerHTML = `<input type="text" placeholder="Star Wars character"/>`
+  function getAPI() {
+    return {
+      search: () => {
+        return fetch('https://swapi.co/api/people/?search=r2')
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+            return data.results
+          })
+      }
     }
-  })
+  }
+
+  function defineCustomElements(api) {
+    window.customElements.define('sw-search-box', class extends HTMLElement {
+      constructor() {
+        super()
+
+        this.addEventListener('submit', (event) => {
+          event.preventDefault()
+          console.log('Submitted!');
+          api.search().then(data => {
+            console.log("Search completed!", data);
+          })
+        })
+      }
+
+      connectedCallback() {
+        this.innerHTML = `<form>
+          <input type="text" placeholder="Star Wars character"/>
+        </form>`
+      }
+    })
+  }
+
+  const api = getAPI()
+  defineCustomElements(api)
+
 })()
