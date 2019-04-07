308f66221763bde516968ba30fafbda96a6e0134
Daniel Moniz <daniel.moniz@gmail.com>
2019-04-07 07:18:45 -0400

    Add very simple web component for search box.

--- components.js
+++ components.js
@@ -1,2 +1,11 @@
 
-console.log('Loaded components!');
+
+(function() {
+  console.log('Loaded components!');
+
+  window.customElements.define('sw-search-box', class extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `<input type="text" placeholder="Star Wars character"/>`
+    }
+  })
+})()
